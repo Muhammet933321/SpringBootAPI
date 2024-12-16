aecd9dfa6e24f89c2bfe030f0fa41530988ad715
Muhammet963321 <78547346+Muhammet933321@users.noreply.github.com>
2024-12-16 14:10:39 +0300

    Try Push

--- src/main/java/com/Blix/UnityAPI/loggers/IpLoggingFilter.java
+++ src/main/java/com/Blix/UnityAPI/loggers/IpLoggingFilter.java
@@ -1,64 +1,36 @@
 package com.Blix.UnityAPI.loggers;
 
-import com.Blix.UnityAPI.entities.IpLog;
-import com.Blix.UnityAPI.entities.User;
-import com.Blix.UnityAPI.repositories.UserRepository;
-import com.Blix.UnityAPI.services.IpLogService;
+import com.Blix.UnityAPI.services.IPLogService;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.Optional;
-
 @Component
-@WebFilter("/*") // Tüm istekleri dinle
 public class IpLoggingFilter implements Filter {
 
     @Autowired
-    private IpLogService ipLogService;
-
-    @Autowired
-    private UserRepository userRepository;
+    private IPLogService ipLogService;
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-        // IP ve URL bilgilerini al
+        // IP Adresi, URL ve karakter bilgisi
         String ipAddress = httpRequest.getRemoteAddr();
         String url = httpRequest.getRequestURI();
+        String characterId = httpRequest.getParameter("characterId");
 
-        // İstekten karakter ID'sini al
-        String characterIdParam = httpRequest.getParameter("characterId");
+        // Loglama
+        ipLogService.logRequest(ipAddress, url, characterId);
 
-        // Karakter ID'yi kontrol et
-        if (characterIdParam != null) {
-            try {
-                Long characterId = Long.parseLong(characterIdParam);
-
-                // Karakter ID'ye sahip kullanıcıyı bul
-                Optional<User> user = userRepository.findById(characterId);
-                if (user.isPresent()) {
-                    ipLogService.saveIpLog(ipAddress, url, user.get());
-                } else {
-                    ipLogService.saveIpLog(ipAddress, url, null); // Kullanıcı bulunamadıysa logla
-                }
-            } catch (NumberFormatException e) {
-                ipLogService.saveIpLog(ipAddress, url, null); // Geçersiz karakter ID'si
-            }
-        } else {
-            // Karakter ID yoksa "NotAuth" olarak logla
-            ipLogService.saveIpLog(ipAddress, url, null);
+        try {
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        // İsteği zincire gönder
-        chain.doFilter(request, response);
     }
 }
